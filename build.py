4ae266432c69ebf63ae12abdf3db0c447a4c52af
sub1kore <115646511+sub1kore@users.noreply.github.com>
2026-06-26 11:05:45 +0330

    Update build.py

--- build.py
+++ build.py
@@ -11,3 +11,24 @@ if cfg.exists():
 Path("sub.txt").write_text("\n".join(lines),encoding="utf-8")
 Path("sub_base64.txt").write_text(base64.b64encode("\n".join(lines).encode()).decode(),encoding="utf-8")
 print("Done")
+# نام برند
+BRAND = "⚡ Sub1Kore VPN"
+
+new_configs = []
+
+for config in configs:
+    config = config.strip()
+    if not config:
+        continue
+
+    # اگر کانفیگ نام داشته باشد
+    if "#" in config:
+        link, name = config.rsplit("#", 1)
+
+        # جلوگیری از اضافه شدن دوباره برند
+        if not name.startswith(BRAND):
+            config = f"{link}#{BRAND} | {name}"
+
+    new_configs.append(config)
+
+configs = new_configs
