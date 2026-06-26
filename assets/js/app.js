7321b72ce939c203c5528ce327cadb71f87f3f24
sub1kore <115646511+sub1kore@users.noreply.github.com>
2026-06-26 13:50:01 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
 fetch("data/stats.json")
-  .then(res => res.json())
-  .then(data => {
+.then(res => res.json())
+.then(data => {
+
     document.getElementById("brand").textContent = data.brand;
+
     document.getElementById("nodes").textContent = data.total_lines;
+
     document.getElementById("updated").textContent = data.updated;
+
     document.getElementById("protocol").textContent =
-      Object.keys(data.protocols).join(", ").toUpperCase();
-  })
-  .catch(err => console.error("Error loading stats:", err));
+        Object.keys(data.protocols)
+              .map(p => p.toUpperCase())
+              .join(", ");
+
+    document.getElementById("copy").onclick = () => {
+
+        navigator.clipboard.writeText(
+            location.origin + "/v2ray-sub/sub_base64.txt"
+        );
+
+        alert("Subscription copied!");
+
+    };
+
+});
